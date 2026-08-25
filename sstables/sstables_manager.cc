2d92951aba07d82992e7827fb7d9b7fec5c20b57
Andrzej Jackowski <andrzej.jackowski@scylladb.com>
2026-08-25 13:49:49 +0200

    Revert "reader_concurrency_semaphore: enable 50% shared pool"
    This reverts merge commit 838ef139c23a17ef903e24c8ac1e901dc92c1d1d,
    which merged scylladb/scylladb#28438 into
    e41dfc34920f748f8dee337099c437e3b907e326.

--- sstables/sstables_manager.cc
+++ sstables/sstables_manager.cc
@@ -100,8 +100,7 @@ sstables_manager::sstables_manager(
         utils::updateable_value(std::numeric_limits<uint32_t>::max()),
         utils::updateable_value(uint32_t(1)),
         utils::updateable_value(0.0f),
-        reader_concurrency_semaphore::register_metrics::no,
-        reader_concurrency_semaphore_shared_pool::empty_pool())
+        reader_concurrency_semaphore::register_metrics::no)
     , _dir_semaphore(dir_sem)
     , _resolve_host_id(std::move(resolve_host_id))
     , _maintenance_sg(std::move(maintenance_sg))
